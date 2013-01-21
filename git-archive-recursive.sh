2485a83f7a8f1c700052f84ede03fba96398cd10
Benjamin Schuster-Boeckler <ben@mekentosj.com>
2013-01-21 14:18:09 +0000

    Working on updating this script for newer git versions that don't keep the object files in the submodules .git directory. It's still not working, though, some problem with the `git submodule foreach` changing environment variables I think.

--- git-archive-recursive.sh
+++ git-archive-recursive.sh
@@ -1,34 +1,71 @@
-#!/bin/sh
+#!/bin/bash
+#---------------- functions ----------------
+function do_or_die {
+	"$@"
+	status=$?
+	if [ $status -ne 0 ]; then
+		echo "Error executing $1" 1>&2
+		exit $?
+	fi
+	return $status
+}
 
-export revision="$1"
+function read_one_level () {
+	export GIT_ALTERNATE_OBJECT_DIRECTORIES="$GIT_ALTERNATE_OBJECT_DIRECTORIES":$(
+		git submodule foreach '
+			if [ -d "$up/$path/.git" ]; then
+				echo "$up/$path/.git/objects"
+			else
+				DIR=`cat "$up/$path/.git" | sed "s/gitdir:[[:space:]]*//"`
+				DIR="$up/$path/$DIR/objects"
+				DIR=`cd -P -- "$(dirname -- "$DIR")" && echo "$(pwd -P)/$(basename -- "$DIR")"`
+				echo $DIR
+			fi
+		' |
+		grep -E -v '^(Entering|No submodule mapping found)' |
+		tr '\n' : |
+		sed 's/:$//'
+	)
+	echo "DEBUG $GIT_ALTERNATE_OBJECT_DIRECTORIES" 1>&2
+	
+	do_or_die git submodule --quiet foreach '
+		cd "$toplevel"
+		subcommit=$(git rev-parse :"$path")
+		echo "DEBUG Subcommit for $path in $toplevel: $subcommit" 1>&2
+		git read-tree -i --prefix="$path/" $subcommit
+		if [ $? -eq 0 ]; then
+			echo "git rm --cached $path"
+		else
+			echo "ERROR Failed to export tree" 1>&2
+		fi
+	'
+}
 
+#---------------- main code ----------------
+revision="$1"
+if [ "$revision" == "" ]; then
+	revision="HEAD"
+fi
+up="$2"
+if [ "$up" == "" ]; then
+	up="$(pwd)"
+fi
+
+export revision
 export GIT_INDEX_FILE=".git/tmpindex"
+export up
+
+echo "INFO revision: $revision directory: $up" 1>&2
+
 rm -f "$GIT_INDEX_FILE"
 
 git read-tree $revision
 
-export up="$(pwd)"
-
-read_one_level () {
-	export GIT_ALTERNATE_OBJECT_DIRECTORIES="$GIT_ALTERNATE_OBJECT_DIRECTORIES":$(
-	    git submodule foreach 'echo "$up/$path/.git/objects"' |
-	    grep -E -v '^(Entering|No submodule mapping found)' |
-	    tr '\n' : |
-	    sed 's/:$//'
-	)
-
-	git submodule foreach '
-		cd "$up"
-		subcommit=$(git rev-parse :"$path")
-		git rm --cached "$path"
-		git read-tree -i --prefix="$path/" $subcommit
-	' >/dev/null
-}
-
 while git ls-files -s | grep -q ^160000; do
-    read_one_level
+	echo "INFO Read level: $up" 1>&2
+	read_one_level
 done
 
-git archive --format=tar $(git write-tree)
+git archive --format=zip $(git write-tree)
 
 rm -f "$GIT_INDEX_FILE"
